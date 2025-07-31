9850689e8fe957bdfe0ba13f1873f18ca935d65f
Jignesh Pampaniya <157948618+JIGNESH1409@users.noreply.github.com>
2025-07-31 22:26:32 +0530

    Create SwapNode.java

---
+++ LinkedList/SwapNode.java
@@ -0,0 +1,51 @@
+/*
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode dummy=new ListNode(0,head);
+        ListNode fast=dummy;
+        ListNode slow=dummy;
+        ListNode temp=head;
+        for(int i=1;i<k;i++){
+            temp=temp.next;
+        }
+        for(int i=0;i<k;i++){
+            fast=fast.next;
+        }
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        dummy.val=slow.next.val;
+        slow.next.val=temp.val;
+        temp.val=dummy.val;
+        return head;
+        
+    }
+}
