3296a70cb400536bebcb32f58cdb7666508ffc3c
Jignesh Pampaniya <157948618+JIGNESH1409@users.noreply.github.com>
2025-08-06 18:53:53 +0530

    Create SymmetricTree.java

---
+++ BinaryTree/SymmetricTree.java
@@ -0,0 +1,62 @@
+/*
+
+For More Description refer LeetCode Problem--->101
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+        while(!queue.isEmpty()){
+            TreeNode left=queue.poll();
+            TreeNode right=queue.poll();
+
+            if(left==null && right==null){
+               continue;
+            }
+            if(left==null || right == null){
+                return false;
+            }
+
+
+            if(left.val!=right.val){
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
+    
+    }
+}
