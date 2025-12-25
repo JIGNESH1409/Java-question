3ab0130021675f9b3e36b9caca651dc4dc3a9830
Jignesh Pampaniya <157948618+JIGNESH1409@users.noreply.github.com>
2025-12-25 16:35:22 +0530

    Add solution for finding missing numbers in array
    Implement solution for LeetCode problem 448 to find missing numbers in an array without extra space.

---
+++ Arrays/findMissingNumInPlaceArray.java
@@ -0,0 +1,46 @@
+/*
+
+LeetCode problem-448
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+ 
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+*/
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer>  res = new ArrayList<>();
+        int value;
+        for(int i=0;i<nums.length;i++){
+            value=Math.abs(nums[i]);
+            if(nums[value-1]>0){
+                nums[value-1]=-nums[value-1];
+            }
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]>0){
+                res.add(j+1);
+            }
+        }
+        return res;
+
+    }
+}
